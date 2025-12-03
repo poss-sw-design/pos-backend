a9220f5af852df3a2b31843c68f34bef196f2fcd
차예원 <chayewon@chayewon-ui-noteubug.local>
2025-12-03 17:46:36 +0200

    feat: Add basic Spring Security configuration

--- src/main/java/com/pos/backend/domain/Product.java
+++ src/main/java/com/pos/backend/domain/Product.java
@@ -1,4 +1,20 @@
 package com.pos.backend.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+  private Integer price;
+  private String category;
+  private Long merchantId;
 }
